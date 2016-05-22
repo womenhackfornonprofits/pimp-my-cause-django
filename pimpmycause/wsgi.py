380ecc855c6b97904f54b13def64fc1f079dee9b
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-05-22 20:41:29 +0100

    removed settings

--- pimpmycause/wsgi.py
+++ pimpmycause/wsgi.py
@@ -12,9 +12,6 @@ from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pimpmycause.settings")
-
-application = get_wsgi_application()
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
