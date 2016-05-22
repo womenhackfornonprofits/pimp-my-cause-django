dfe1af0b9ab4a893bd93ee5f48ba237a751a9223
Elianne Schutze <eliannemsr@gmail.com>
2016-05-22 15:20:40 +0100

    model fix

--- pimpmycause/profiles/models.py
+++ pimpmycause/profiles/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     #firstname
+    firstname = models.CharField(max_length=70)
     #surname
     #email
     #telephone
