8ea78809cd84041f2ea63d7446d847f3c45e4343
Elianne Schutze <eliannemsr@gmail.com>
2016-05-22 15:16:34 +0100

    test push, create User model

--- pimpmycause/profiles/models.py
+++ pimpmycause/profiles/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    #firstname
+    #surname
+    #email
+    #telephone
+    #country
+    #city
+    #postcode
+    #password
+    #position
+    #usertype
+    #twitter
