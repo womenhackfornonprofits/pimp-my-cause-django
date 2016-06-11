59694ecea7f6ed60f318031d81f0b50c599e9c0e
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-06-12 00:27:20 +0100

    Swapped reset for normalize, removed more jquery vendor js files, commented out some components to refactor

---
+++ frontend/src/scripts/slider.js
@@ -0,0 +1,24 @@
+$(window).on("load", function() {
+    
+ // THIS IS BUGGY
+
+//  *******************************
+//  FEATURED CAUSES/MARKETERS PANEL 
+//  ******************************* 
+        
+// Fade Responsive Slider
+    
+    $('.fade').slick({
+        autoplay: true,
+        autoplaySpeed: 800,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+    
+});
