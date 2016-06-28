a3f694a2f02fe614c56460566eb083fce79635a2
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-06-28 21:31:11 +0100

    Added a new view and template for About Us page

--- pimpmycause/core/views.py
+++ pimpmycause/core/views.py
@@ -5,13 +5,19 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomepageView(TemplateView):
     """
-    The Homepage view.
+    The Homepage View.
     """
     template_name = "index.html"
 
 
 class ContactView(TemplateView):
     """
-    The Homepage view.
+    The Contact Us View.
     """
     template_name = "core/contact.html"
+
+class AboutView(TemplateView):
+    """
+    The About Us view.
+    """
+    template_name = "core/about.html"
