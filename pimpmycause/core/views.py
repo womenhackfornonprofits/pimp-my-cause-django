363fea71d3174aba7d2b94213f63d20f3a6621ee
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-08-08 20:39:21 +0100

    Fixed view descriptions

--- pimpmycause/core/views.py
+++ pimpmycause/core/views.py
@@ -2,64 +2,64 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-# Create your views here.
 class HomepageView(TemplateView):
     """
-    The Homepage View.
+    Homepage view.
     """
     template_name = "index.html"
 
 
 class ContactView(TemplateView):
     """
-    The Contact Us View.
+    Contact Us view.
     """
     template_name = "core/contact.html"
 
 
 class AboutView(TemplateView):
     """
-    The About Us View.
+    About Us view.
     """
     template_name = "core/about.html"
 
 
 class WhoWeAre(TemplateView):
     """
-    The About Us View.
+    Who We Are view.
     """
     template_name = "core/who-we-are.html"
 
 
-
 class HowItWorks(TemplateView):
     """
-    The About Us View.
+    How it works view.
     """
     template_name = "core/how-it-works.html"
 
 
-
 class MeetTheTeam(TemplateView):
     """
-    Meet The Team View.
+    Meet The Team view.
     """
     template_name = "core/team.html"
 
+
 class Partnerships(TemplateView):
     """
-    Meet The Team View.
+    Partnerships view.
     """
     template_name = "core/partnerships.html"
 
+
 class OurPartners(TemplateView):
     """
-    Meet The Team View.
+    Our Partners view.
     """
     template_name = "core/our-partners.html"
 
+
 class BecomeAPartner(TemplateView):
     """
-    Meet The Team View.
+    Become A Partner view.
     """
     template_name = "core/become-a-partner.html"
