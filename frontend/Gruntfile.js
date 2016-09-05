36597dd7e9b19bdc7b955a120710a7640b908b06
Van <singularity@vannio.co.uk>
2016-09-06 00:49:31 +0100

    Tabs to spaces for readability on github

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,76 +1,76 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-	//------- CSS Minify -------//
-	cssmin: {
-		combine: {
-		  files: {
-		    '../pimpmycause/static/styles/styles.css': ['css/styles.css']
-		  }
-		}
-	},
-
-	//------- SASS -------//
-	sass: {
-		dist: {
-			files: {
-				'css/styles.css': 'src/sass/styles.scss'
-			}
-		}
-	},
-
-	//------- Watch SASS -> CSS -------//
-	watch: {
-		sass: {
-		  files: 'src/sass/**/**.scss',
-		  tasks: ['sass', 'cssmin']
-		}
-	},
-
-	jspaths: {
-    src: {
-      js: 'src/**/**.js'
+    //------- CSS Minify -------//
+    cssmin: {
+      combine: {
+        files: {
+          '../pimpmycause/static/styles/styles.css': ['css/styles.css']
+        }
+      }
     },
-    dest: {
-      jsMin: '../pimpmycause/static/scripts/pimpmycause.min.js'
-    }
-  },
 
-  //------- JS Minify ------//
-  uglify: {
-    options: {
-      compress: true,
-      mangle: true,
-      sourceMap: true
+    //------- SASS -------//
+    sass: {
+      dist: {
+        files: {
+          'css/styles.css': 'src/sass/styles.scss'
+        }
+      }
     },
-    target: {
-      src: '<%= jspaths.src.js %>',
-      dest: '<%= jspaths.dest.jsMin %>'
-    }
-  },
 
-  //------- copy remaining static files ------//
-  copy: {
-    img: {
-      files: [{
-        expand: true,
-        nonull: true,
-        cwd: 'src/img',
-        src: ['*.{png,jpg,jpeg,svg,gif}'],
-        dest: '../pimpmycause/static/img/',
-        filter: 'isFile'
-      }, ]
-    }
-  },
+    //------- Watch SASS -> CSS -------//
+    watch: {
+      sass: {
+        files: 'src/sass/**/**.scss',
+        tasks: ['sass', 'cssmin']
+      }
+    },
 
-});
+    jspaths: {
+      src: {
+        js: 'src/**/**.js'
+      },
+      dest: {
+        jsMin: '../pimpmycause/static/scripts/pimpmycause.min.js'
+      }
+    },
+
+    //------- JS Minify ------//
+    uglify: {
+      options: {
+        compress: true,
+        mangle: true,
+        sourceMap: true
+      },
+      target: {
+        src: '<%= jspaths.src.js %>',
+        dest: '<%= jspaths.dest.jsMin %>'
+      }
+    },
+
+    //------- copy remaining static files ------//
+    copy: {
+      img: {
+        files: [{
+          expand: true,
+          nonull: true,
+          cwd: 'src/img',
+          src: ['*.{png,jpg,jpeg,svg,gif}'],
+          dest: '../pimpmycause/static/img/',
+          filter: 'isFile'
+        }]
+      }
+    }
+
+  });
 
   grunt.registerTask('default', ['sass', 'cssmin', 'uglify', 'copy']);
 
