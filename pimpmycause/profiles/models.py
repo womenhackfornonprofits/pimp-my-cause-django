14e93fef6e5df94f57db496fb4d9588a0abe5a7a
Yana <gbartzi@windowslive.com>
2016-09-27 00:12:01 +0100

    new field for cause categories

--- pimpmycause/profiles/models.py
+++ pimpmycause/profiles/models.py
@@ -82,9 +82,36 @@ class MarketerProfile(models.Model):
 
 class CauseProfile(models.Model):
 
+    DISASTER_RELIEF = 0
+    POVERTY_ALLEVIATION = 1
+    ENVIRONMENT = 2
+    EDUCATION_AND_SKILLS = 3
+    ANIMAL_WELFARE = 4
+    CHILDREN_AND_YOUTH = 5
+    DISABILITY = 6
+    HEALTH = 7
+    FOOD = 8
+    FAMILY_SUPPORT = 9
+    CARE_FOR_THE_ELDERY = 10
+
+    CAUSE_CATEGORY_CHOICES = (
+        (DISASTER_RELIEF, "Disaster Relief"),
+        (POVERTY_ALLEVIATION, "Poverty Alleviation"),
+        (ENVIRONMENT, "Environment"),
+        (EDUCATION_AND_SKILLS, "Education and Skills"),
+        (ANIMAL_WELFARE, "Animal Welfare"),
+        (CHILDREN_AND_YOUTH, "Children and Youth"),
+        (DISABILITY, "Disability"),
+        (HEALTH, "Health"),
+        (FOOD, "Food"),
+        (FAMILY_SUPPORT, "Family Support"),
+        (CARE_FOR_THE_ELDERY, "Care for the Eldery")
+    )
+
     profile = models.ForeignKey(
         PimpUser,
         limit_choices_to={'usertype': PimpUser.CAUSE }
     )
 
     mission = models.CharField(max_length=1000, blank=True)
+    categories = models.CharField(max_length=8, choices=CAUSE_CATEGORY_CHOICES, blank=True)
