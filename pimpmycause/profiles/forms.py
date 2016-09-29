1fbe82ae166ecbb9686465bb73bf221bb0027dcf
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-09-29 21:58:39 +0100

    Adding a button to use as file uploader

--- pimpmycause/profiles/forms.py
+++ pimpmycause/profiles/forms.py
@@ -29,4 +29,19 @@ class MarketerProfileForm(forms.ModelForm):
 
 class S3DirectUploadForm(forms.Form):
 
-    image = forms.URLField(widget=S3DirectWidget(dest='user-profile-images'))
+    #image = forms.URLField(widget=S3DirectWidget(dest='user-profile-images'))
+    images = forms.URLField(widget=S3DirectWidget(
+        dest='user-profile-images',
+        html=(
+            '<div class="s3direct" data-policy-url="{policy_url}">'
+            '  <a class="file-link" target="_blank" href="{file_url}">{file_name}</a>'
+            '  <a class="file-remove" href="#remove">Remove</a>'
+            '  <input class="file-url" type="hidden" value="{file_url}" id="{element_id}" name="{name}" />'
+            '  <input class="file-dest" type="hidden" value="{dest}">'
+            '  <input class="file-input js-input-file inputfile" type="file" id="file" name="file"/>'
+            '  <label for="file" class="button">Choose an image</label>'
+            '  <div class="progress progress-striped active">'
+            '    <div class="bar"></div>'
+            '  </div>'
+            '</div>'
+        )))
