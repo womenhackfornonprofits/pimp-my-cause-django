6408342cabf9f22f55b6943b5ae75f6247d1434c
Lili <kastilio.liliana@gmail.com>
2018-01-09 20:23:21 +0530

    Mobile menu tweaks

--- pimpmycause/src/scripts/components/hamburger-menu.js
+++ pimpmycause/src/scripts/components/hamburger-menu.js
@@ -8,7 +8,9 @@ $navigation.addEventListener('click', (event) => {
 
     if ($srcElement.classList.contains('js-nav-toggle')) {
         $navigation.classList.add('navigation__menu--mobile');
+        document.body.classList.add('nav--active');
     } else {
         $navigation.classList.remove('navigation__menu--mobile');
+        document.body.classList.remove('nav--active');
     }
 });
