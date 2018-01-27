59f2a871f5ce93411f189eb293b2e3e5ba57404f
Lili Kastilio <lili@lilianakastilio.co.uk>
2018-01-27 18:15:08 +0000

    Feature/initial news migration (#94)
    * Fix advert detail view button
    
    * Remove frown icon
    
    * Upgrade django registration, move cause name validation to form
    
    * Restyled the inbox and sent message views
    
    * Upgraded registration redux
    
    * Initial news migration and basic pagination
    
    * Update circle config to include postgresql
    
    * Pagination styles for news page

--- pimpmycause/news/views.py
+++ pimpmycause/news/views.py
@@ -1,3 +1,8 @@
+from django.core.paginator import (
+    Paginator,
+    EmptyPage,
+    PageNotAnInteger
+)
 from django.shortcuts import (
     get_object_or_404,
     render,
@@ -19,10 +24,23 @@ def news_post_detail(request, slug):
 
 def news_post_list(request):
     news_post_list = (
-        NewsPost.objects
-        .order_by('created_at')
+        NewsPost.objects.all()
+        .order_by('-created_at')
     )
+    page = request.GET.get('page')
 
-    context = {'news_post_list': news_post_list}
+    paginator = Paginator(news_post_list, 25)
+    try:
+        news_post_list_paginated = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        news_post_list_paginated = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        news_post_list_paginated = paginator.page(paginator.num_pages)
+
+    # page = request.GET.get('page')
+    # news_post_list_paginated = paginator.get_page(page)
+    context = {'news_post_list': news_post_list_paginated}
 
     return render(request, 'news/news_post_list.html', context)
