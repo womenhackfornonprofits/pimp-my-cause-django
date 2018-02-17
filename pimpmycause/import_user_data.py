0d98425ed62943693a2d96c8121f2fa001997d7d
Lili <kastilio.liliana@gmail.com>
2018-02-17 23:09:11 +0000

    Small fixes and initial challenges page

--- pimpmycause/import_user_data.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('data.json', 'rb') as f:
-    users_data = json.loads(f.read().decode('utf8'))
-
-    for user in users_data:
-        print(user)
--- pimpmycause/search/filters.py
+++ pimpmycause/search/filters.py
@@ -1,7 +1,3 @@
-from django.db import (
-    models,
-)
-from django import forms
 import django_filters
 from django_filters import (
     MultipleChoiceFilter,
