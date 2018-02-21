7f618fa0bc4aa68f48d980aaae4f549082170d65
Lili Kastilio <lili@lilianakastilio.co.uk>
2018-02-21 22:42:03 +0000

    Team memeber user fixtures, added team type field as well

--- pimpmycause/core/admin.py
+++ pimpmycause/core/admin.py
@@ -5,7 +5,7 @@ from models import TeamMember
 
 class TeamMemberAdmin(admin.ModelAdmin):
 
-    list_display = ['name', 'surname', 'position', 'visual_priority']
+    list_display = ['name', 'surname', 'position', 'visual_priority', 'group']
 
 
 admin.site.register(TeamMember, TeamMemberAdmin)
