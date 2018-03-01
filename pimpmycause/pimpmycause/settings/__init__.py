764c251bb5f51b35b2724b5f15a94405346914c1
Lili Kastilio <lili@lilianakastilio.co.uk>
2018-03-01 21:20:15 +0000

    Move the utils to core

--- pimpmycause/pimpmycause/settings/__init__.py
+++ pimpmycause/pimpmycause/settings/__init__.py
@@ -7,7 +7,7 @@ from env_utils import (
     get_env,
 )
 
-from pimpmycause.core.utils import generate_upload_destination_path
+from core.utils import generate_upload_destination_path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
