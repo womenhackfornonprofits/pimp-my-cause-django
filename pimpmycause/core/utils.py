74ff8d968626b3c5520f59ac256df0bf0caa6a1a
Lili Kastilio <lili@lilianakastilio.co.uk>
2018-03-01 21:37:11 +0000

    Script to rename all s3 files to remove spaces

--- pimpmycause/core/utils.py
+++ pimpmycause/core/utils.py
@@ -1,8 +1,8 @@
 import os
 import uuid
+import boto
 
 from django.utils.text import slugify
-
 
 def slugify_filename(filename):
     filename, file_extension = os.path.splitext(filename)
@@ -26,3 +26,18 @@ def generate_upload_destination_path(folder_name):
             filename
         )
     return inner
+
+
+def rename_all_s3_files():
+    AWS_STORAGE_BUCKET_NAME='pimp-my-cause-media-stage'
+    AWS_STORAGE_REGION='eu-west-2'
+
+    conn = boto.s3.connect_to_region(AWS_STORAGE_REGION)
+    bucket = conn.get_bucket(AWS_STORAGE_BUCKET_NAME)
+    print(AWS_STORAGE_REGION)
+    print(AWS_STORAGE_BUCKET_NAME)
+
+    for k in bucket.list():
+        if ' ' in k.key:
+            bucket.copy_key(k.key.replace(' ', '-'), bucket.name, k.key)
+            bucket.delete_key(k.key)
