09e566540f48e264a911c4f5f8b40e76fa95ab63
Lili Kastilio <lili@lilianakastilio.co.uk>
2018-05-16 22:09:23 +0100

    chore: adding more search fields to ads admin

--- pimpmycause/adverts/admin.py
+++ pimpmycause/adverts/admin.py
@@ -7,7 +7,7 @@ from adverts.models import Advert
 class AdvertAdmin(admin.ModelAdmin):
 
     list_select_related = ['cause_profile']
-    list_display = ['title', 'cause_profile', 'created_at']
-
+    list_display = ['title', 'cause_profile', 'created_at', 'deadline']
+    search_fields = ['description', 'title', 'cause_profile__cause_name']
 
 admin.site.register(Advert, AdvertAdmin)
