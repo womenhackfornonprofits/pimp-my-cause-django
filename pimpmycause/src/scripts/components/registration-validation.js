36e4672846c54423ce5581c5a56a529230365fe7
Lili Kastilio <lili@lilianakastilio.co.uk>
2021-10-04 22:15:00 +0100

    feat: make Cause name required for causes (#181)

--- pimpmycause/src/scripts/components/registration-validation.js
+++ pimpmycause/src/scripts/components/registration-validation.js
@@ -15,9 +15,13 @@ function setUserType(userTypeValue = 0) {
 
 function hideShowCauseName() {
     if ($userTypeEl.value === '0') {
+        $causeNameEl.parentElement.classList.add('visuallyhidden')
         $causeNameEl.classList.add('visuallyhidden');
+        $causeNameEl.removeAttribute('required');
     } else {
+        $causeNameEl.parentElement.classList.remove('visuallyhidden')
         $causeNameEl.classList.remove('visuallyhidden');
+        $causeNameEl.setAttribute('required',true);
     }
 }
 
