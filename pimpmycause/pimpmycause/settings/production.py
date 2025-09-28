b468e1610c46cb815960a290b6fc7b7bab4e104e
Lili Kastilio <lili@lilianakastilio.co.uk>
2025-09-28 16:20:11 +0100

    feat: upgrade to python 3.11

--- pimpmycause/pimpmycause/settings/production.py
+++ pimpmycause/pimpmycause/settings/production.py
@@ -5,6 +5,10 @@ from env_utils import (
 )
 DEBUG = False
 
+import os
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
