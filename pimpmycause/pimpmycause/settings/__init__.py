fc5dfb15ea36974afdeec4ab94fac9b3c1c43603
Lili Kastilio <lili@snyk.io>
2025-10-27 17:56:06 +0000

    Use StaticFilesStorage instead of CompressedStaticFilesStorage

--- pimpmycause/pimpmycause/settings/__init__.py
+++ pimpmycause/pimpmycause/settings/__init__.py
@@ -168,7 +168,7 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'profiles.PimpUser'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 STATIC_URL = '/staticfiles/'
 
