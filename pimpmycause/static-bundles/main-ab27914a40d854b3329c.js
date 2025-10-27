dec93fd115c3840622683b01f0d6ea60242f28ce
Lili Kastilio <lili@snyk.io>
2025-10-27 18:16:53 +0000

    Fix hamburger menu null pointer error in compiled bundle

--- pimpmycause/static-bundles/main-ab27914a40d854b3329c.js
+++ pimpmycause/static-bundles/main-ab27914a40d854b3329c.js
@@ -217,14 +217,18 @@ const $ = __webpack_require__(0);
 
 const $navigation = $('.js-header-nav')[0];
 
+if (!$navigation) return;
+
 $navigation.addEventListener('click', event => {
 
     const $srcElement = event.target;
     let $currentToggle = $srcElement;
 
-    while (!$currentToggle.classList.contains('js-nav-toggle')) {
+    while ($currentToggle && !$currentToggle.classList.contains('js-nav-toggle')) {
         $currentToggle = $currentToggle.parentElement;
     }
+
+    if (!$currentToggle) return;
 
     if (document.body.className.match('nav--active')) {
         $navigation.classList.remove('navigation__menu--mobile');
