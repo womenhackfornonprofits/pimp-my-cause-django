139f2ae9d0d66a2a4f1e8222e3452287dfffc99a
Lili Kastilio <lili@snyk.io>
2025-10-27 18:13:09 +0000

    Fix hamburger menu null pointer error

--- pimpmycause/src/scripts/components/hamburger-menu.js
+++ pimpmycause/src/scripts/components/hamburger-menu.js
@@ -3,15 +3,19 @@ const $ = require('qwery');
 
 const $navigation = $('.js-header-nav')[0];
 
+if (!$navigation) return;
+
 $navigation.addEventListener('click', (event) => {
 
     const $srcElement = event.target;
     let $currentToggle = $srcElement;
 
 
-    while (!$currentToggle.classList.contains('js-nav-toggle')) {
+    while ($currentToggle && !$currentToggle.classList.contains('js-nav-toggle')) {
         $currentToggle  = $currentToggle.parentElement;
     }
+
+    if (!$currentToggle) return;
 
     if (document.body.className.match('nav--active')) {
         $navigation.classList.remove('navigation__menu--mobile');
